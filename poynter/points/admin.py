955e96a38b5aaf3408b6f953784452e28d663524
Scot Hacker <shacker@birdhouse.org>
2025-06-23 22:46:56 -0700

    FK sessions to projects

--- poynter/points/admin.py
+++ poynter/points/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import PointingSession, Ticket, Vote
+from .models import PointingSession, Ticket, Vote, Project
 
 
 class VoteAdmin(admin.ModelAdmin):
@@ -10,9 +10,10 @@ class TicketAdmin(admin.ModelAdmin):
     list_display = ("title", "pointing_session", "created")
 
 class PointingSessionAdmin(admin.ModelAdmin):
-    list_display = ("moderator", "session_date", )
+    list_display = ("moderator", "project", "session_date", )
 
 
 admin.site.register(Vote, VoteAdmin)
 admin.site.register(PointingSession, PointingSessionAdmin)
 admin.site.register(Ticket, TicketAdmin)
+admin.site.register(Project)
--- poynter/points/models.py
+++ poynter/points/models.py
@@ -4,8 +4,20 @@ from django.db import models
 from django_extensions.db.models import TimeStampedModel
 
 
+
+class Project(TimeStampedModel):
+    """Voting Sessions are associated with projects within the organization."""
+
+    name = models.name = models.CharField(
+        max_length=120,
+        help_text="A project title within the organization.",
+    )
+
+    def __str__(self):
+        return f"{self.name}"
+
 class Vote(TimeStampedModel):
-    """ One user votes on one issue"""
+    """One user's vote on one ticket."""
     voter = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     vote = models.SmallIntegerField(help_text="Numerical vote up to 2 digits")
 
@@ -14,13 +26,15 @@ class Vote(TimeStampedModel):
 
 
 class PointingSession(TimeStampedModel):
-    """ A session is a meeting in which users vote on stories.
+    """A session is a meeting in which users vote on stories.
     A PointingSession has a moderator and a collection of stories.
     TODO: Allow NOW or arbitrary date
     """
 
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
     moderator = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    session_date = models.DateTimeField("date logged")
+    session_date = models.DateTimeField("Voting window")
+    is_open = models.BooleanField(help_text="Voting is currently open", default=False)
 
     def __str__(self):
         return f"{self.moderator.username}: {self.session_date.strftime('%A, %d %B, %Y at %X')}"
@@ -29,23 +43,26 @@ class PointingSession(TimeStampedModel):
         verbose_name = "Voting Session"
         verbose_name_plural = "Voting Sessions"
 
+
 class Ticket(TimeStampedModel):
-    """ A ticket reference (e.g. to Jira), to be stored in a PointingSession and voted on by users. """
+    """A ticket reference (e.g. to Jira), to be stored in a PointingSession and voted upon by users."""
+
     url = models.URLField(help_text="Link into ticket system", verbose_name="URL")
-    title = models.name = models.CharField(max_length=120, blank=True, help_text="Extracted automatically if possible, or populate manually.")
+    title = models.name = models.CharField(
+        max_length=120,
+        blank=True,
+        help_text="Extracted automatically if possible, or populate manually.",
+    )
     pointing_session = models.ForeignKey(PointingSession, on_delete=models.CASCADE)
 
     def __str__(self):
         return f"{self.pk}: {self.title}"
 
-
     def save(self, *args, **kwargs):
         """Try to populate the title automatically. If we can't get to the remote system,
-        we can still enter the title manually. """
+        we can still enter the title manually."""
         if not self.title:
-            headers = {}  # Need this?
-            page = requests.get(self.url, headers=headers)
+            page = requests.get(self.url)
             text = page.text
-            self.title = text[text.find('<title>') + 7 : text.find('</title>')]
+            self.title = text[text.find("<title>") + 7 : text.find("</title>")]
         return super(Ticket, self).save(*args, **kwargs)
-
